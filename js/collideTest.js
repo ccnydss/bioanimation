1f5bbfc0a1838bdd141078b5ff690ec18553e9c7
Ahalya Sanjiv <ahalyasanjiv@gmail.com>
2018-02-16 13:39:39 -0500

    Added multiple particles and wall

--- js/collideTest.js
+++ js/collideTest.js
@@ -1,6 +1,9 @@
+var circles;
 var outerBox;
-var numParticles = 1;
+var numParticles = 4;
 var particles = [];
+var radius = 50;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,25 +18,45 @@ function setup() {
   outerBox = new Container( topLeft, topRight, botRight, botLeft );
   outerBox.draw();
 
-  var velocity = createVector(-5, 3);
+  var velocity = createVector(-2.5, -2);
 
+  // inner box - (x > outerBox.tl.x+50 || x < outerBox.br.x - 50) && (y > outerBox.tl.y+50 || y < outerBox.bl.y-50 )
+  // maxX = outerBox.br.x-52;
+  // minX = outerBox.tl.x+52;
+  // maxY = outerBox.bl.y-52;
+  // minY = outerBox.tl.y+52;
+
+  xRange = outerBox.tr.x - outerBox.tl.x - 100
+  yRange = outerBox.br.y - outerBox.tr.y - 100
   for (var i = 0; i < numParticles; i++) {
-    var radius = 50;
-    particles.push(new Particle( (outerBox.tl.x + radius + 50), outerBox.tl.y + radius+1, 2*radius, velocity) );
+    // Get random location
+    randomX = outerBox.tl.x + radius + (Math.floor(Math.random() * xRange))
+    randomY = outerBox.tl.y + radius + (Math.floor(Math.random() * yRange))
+    console.log(randomX,randomY);
+    //particles.push(new Particle( (outerBox.tl.x + radius + 50), outerBox.tl.y + radius+1, 2*radius, velocity) );
+    
+    particles.push(new Particle(randomX,randomY,2*radius,velocity));
   }
 }
 
 function draw() {
   clear();
   console.log("---------------------- CLEARED ------------------------------- ", frameCount);
+  drawCellWall(outerBox.tl.x,outerBox.tl.y,outerBox.tr.x,outerBox.bl.y);
 
 
   stroke(15, 15, 15, 100);
   fill(15, 15, 15, 100);
+  // line(outerBox.tl.x+50,outerBox.tl.y+50, outerBox.tr.x-50, outerBox.tl.y+50);
   outerBox.draw();
+
+  var randomColor = getRandomColor();
   for (var i = 0; i < numParticles; i++) {
+    fill(color(50, 55, 100));
+
     particles[i].move(outerBox);
   }
+
 }
 
 function Point (_x, _y) {
@@ -66,6 +89,7 @@ function Particle (_x, _y, _diam, _vel) {
   this.move_velocity = createVector(_vel.x, _vel.y);
 
   this.draw = function( xc = this.x, yc = this.y, rc = this.r) {
+    
     ellipse( xc, yc, rc );
   }
 
@@ -84,7 +108,8 @@ function Particle (_x, _y, _diam, _vel) {
     var nextPastTop = this.y + this.move_velocity.y - this.r < container_context.tl.y;
     var nextPastRight = this.x + this.move_velocity.x + this.r > container_context.br.x;
     var nextPastLeft = this.x + this.move_velocity.x - this.r < container_context.bl.x;
-
+    console.log("X", container_context.bl.x, "Y", container_context.tl.y);
+    console.log("X:", this.x, "Y:", this.y, "Radius", this.r);
     while ( nextPastBottom ) {
       console.log("\nAH! BOTTOM------------------------------------------");
       console.log ("MOVE VEC --- ", this.move_velocity.toString());
@@ -166,7 +191,7 @@ function Particle (_x, _y, _diam, _vel) {
       this.x += this.move_velocity.x;
       this.y += this.move_velocity.y;
 
-    } else if ( pastTop ) {
+    } if ( pastTop ) {
       print( "pt" );
 
       // Create new velocity vector based off of reflection
@@ -179,7 +204,7 @@ function Particle (_x, _y, _diam, _vel) {
       this.x += this.move_velocity.x;
       this.y += this.move_velocity.y;
 
-    } else if ( pastRight ) {
+    } if ( pastRight ) {
       print( "pr" );
 
       console.log("ONE", this.orig_velocity.toString());
@@ -195,7 +220,7 @@ function Particle (_x, _y, _diam, _vel) {
       this.x += this.move_velocity.x;
       this.y += this.move_velocity.y;
 
-    } else if ( pastLeft ) {
+    } if ( pastLeft ) {
       print( "pl" );
 
       // Create new velocity vector based off of reflection
@@ -239,3 +264,20 @@ function keyPressed() {
     toggleLoop();
   }
 }
+
+function getRandomColor() {
+  randomColor = [Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255)];
+  return randomColor;
+}
+
+function drawCellWall(x1,y1,x2,y2) {
+  var channelGap = 100;
+  var wallThickness = 50;
+  boxWidth = x2 - x1;
+  boxHeight = y2 - y1;
+  background(255);
+  fill(0);
+  rect(x1, y1 + boxHeight/2, (boxWidth/2) - (channelGap/2), wallThickness, 0, 10, 10, 0);
+  rect(x1 + (boxWidth/2) + (channelGap/2), y1 + boxHeight/2, (boxWidth/2) - (channelGap/2), wallThickness, 10, 0, 0, 10);
+}
+
