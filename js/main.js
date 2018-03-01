3facb7b606cf3def7e289da47becd5051e40b27f
CC <qwe.com>
2018-03-01 16:55:09 -0500

    Cell wall UI

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 var circles;
 var outerBox = [];
 
+
 var numParticles = [];
 numParticles[0] = 3;
 numParticles[1] = 2;
+//0 = top
+//1 = bot
 
-var OldnumParticles = [];
-OldnumParticles[0] = 3;
-OldnumParticles[1] = 2;
+//For overall particles on the screen
+// var OldnumParticles = [];
+// OldnumParticles[0] = 3;
+// OldnumParticles[1] = 2;
 
 var numParticlesMax = [];
 numParticlesMax[0] = 100;
 numParticlesMax[1] = 100;
 
-var particles0 = [];
-var particles1 = [];
+
+//For local particles on each box
+var MaxParticles = 25;
+var NaParticles0 = [];
+var NaParticles1 = [];
+var ClParticles0 = [];
+var ClParticles1 = [];
+//0 = top
+//1 = bot
 
 var cellWalls = [];
 var Channels = [];
@@ -90,12 +101,13 @@ function setup() {
       randomX = outerBox[j].tl.x + radius + (Math.floor(Math.random() * xRange));
       randomY = outerBox[j].tl.y + radius + (Math.floor(Math.random() * yRange));
 
+
       var chance = Math.random()
       if (chance < 0.5) {
-      eval("particles" + j).push(new Na(randomX,randomY,radius,velocity));
+      eval("NaParticles" + j).push(new Na(randomX,randomY,radius,velocity));
       N_Na[j] = N_Na[j] + 1;
       }else {
-      eval("particles" + j).push(new Cl(randomX,randomY,2*radius,velocity));
+      eval("ClParticles" + j).push(new Cl(randomX,randomY,2*radius,velocity));
       N_Cl[j] = N_Cl[j] + 1;
       }
     }
@@ -139,7 +151,10 @@ function setup() {
 
 
     input[k] = createInput(Value);
-    input[k].id("fasf");
+
+    if (Value == 0) {input[k].value(0)}
+    input[k].input(ChangeNumParticles);
+    input[k].id(k);
     input[k].class('qoptions');
     input[k].parent(eval("control" + k));
 
@@ -177,79 +192,14 @@ function draw() {
   for (var j = 0; j < numContainer; j++) {
     for (var i = 0; i < numParticles[j]; i++) {
 
-            eval("particles" + j)[i].color();
-      eval("particles" + j)[i].move(outerBox[j]);
+      if(eval("NaParticles" + j)[i]) {
+              eval("NaParticles" + j)[i].color();
+        eval("NaParticles" + j)[i].move(outerBox[j]);
+      }
+      if(eval("ClParticles" + j)[i]) {
+            eval("ClParticles" + j)[i].color();
+      eval("ClParticles" + j)[i].move(outerBox[j]);
+      }
     }
   }
-
-  //UI
-  // for (var j = 0; j < numContainer*row; j++) {
-  //     var k;
-  //   if (j==1) {
-  //     k = 0;
-  //   } else if (j==4) {
-  //     k = 1;
-  //   }
-  //
-  //   if (input[k].value() >= numParticlesMax[k]) {
-  //     input[k].value(numParticlesMax[k]-1)
-  //   } else if (!input[k].value()) {
-  //     input[k].value(0)
-  //   }
-  //
-  //   if (OldnumParticles[k] != input[k].value() & input[k].value() <= numParticlesMax[k]) {
-  //
-  //
-  //       var velocity = createVector(-5, -4);
-  //
-  //
-  //     if (OldnumParticles[k] > input[k].value()) { //Deleting particles
-  //           eval("particles" + k).splice(input[k].value(), OldnumParticles[k]-input[k].value());
-  //     }
-  //
-  //
-  //     if (OldnumParticles[k] < input[k].value()) { //adding particles
-  //       for (var i = OldnumParticles[k]; i < input[k].value(); i++) {
-  //         randomX = outerBox[k].tl.x + radius + (Math.floor(Math.random() * xRange))
-  //         randomY = outerBox[k].tl.y + radius + (Math.floor(Math.random() * yRange))
-  //         var chance = Math.random()
-  //         if (chance < 0.5) {
-  //         eval("particles" + k).push(new Na(randomX,randomY,radius,velocity));
-  //         }else {
-  //         eval("particles" + k).push(new Cl(randomX,randomY,2*radius,velocity));
-  //         }
-  //       }
-  //     }
-  //     // if (OldnumParticles[k] > input[k].value()) {
-  //     //   var high = OldnumParticles[k];
-  //     //   var low = input[k].value();
-  //     // }
-  //     // else {
-  //     //   var low = OldnumParticles[k];
-  //     //   var high = input[k].value();
-  //     // }
-  //     //
-  //     // for (var i = low; i < high; i++) {
-  //     //   console.log(i+"&&&"+randomX);
-  //     //   randomX = outerBox[k].tl.x + radius + (Math.floor(Math.random() * xRange))
-  //     //   randomY = outerBox[k].tl.y + radius + (Math.floor(Math.random() * yRange))
-  //     //
-  //     //   if (i < numParticlesMax[k]) {
-  //     //     eval("particles" + k)[i].x = randomX;
-  //     //     eval("particles" + k)[i].y = randomY;
-  //     //   }
-  //     // }
-  //
-  //     numParticles[k] = input[k].value();
-  //     OldnumParticles[k] = input[k].value();
-  //   }
-  //
-  //   // var j = k*1/2;
-  //
-  //   // MinusButton[k].position(stage.position().x + 3*(stage.size().width - 600 - 10)/5, stage.position().y + stage.size().height/20 + stage.size().height*j);
-  //   // PlusButton[k].position(stage.position().x + 2*(stage.size().width - 600 - 10)/5, stage.position().y + stage.size().height/20 + stage.size().height*j);
-  //   // input[k].position(stage.position().x + 192, stage.position().y + stage.size().height/20 + 30 + 2 + stage.size().height*j);
-  //   // textboard[k].position(stage.position().x + (stage.size().width - 600 - 10)/5, stage.position().y + stage.size().height/20 + 30 + stage.size().height*j);
-  // }
-  // UI
 }
