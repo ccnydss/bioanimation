77f5b9f074c8169ebfa0c13de772d9b8c8329125
Chen <chen0gao@gmail.com>
2019-01-30 13:11:24 -0500

    Detecting Mobile and display the message #43

--- js/browserDetection.js
+++ js/browserDetection.js
@@ -1,19 +1,43 @@
+// https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+  platform = window.navigator.platform,
+  macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+  windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+  iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+  os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
 // https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
 // https://github.com/processing/p5.js/blob/master/developer_docs/supported_browsers.md
 browserVersion= (function(){
-    var ua= navigator.userAgent, tem,
-    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
-        return 'Internet Explorer '+(tem[1] || '');
-    }
-    if(M[1]=== 'Chrome'){
-        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
-        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
-    }
-    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
-    return M;
+  var ua= navigator.userAgent, tem,
+  M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if(/trident/i.test(M[1])){
+    tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return 'Internet Explorer '+(tem[1] || '');
+  }
+  if(M[1]=== 'Chrome'){
+    tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+    if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+  }
+  M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+  if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+  return M;
 })();
 
 if(browserVersion.indexOf(',') >= 0)
@@ -21,8 +45,8 @@ browserVersion = browserVersion.split(',');
 if(browserVersion.indexOf(' ') >= 0)
 browserVersion = browserVersion.split(' ');
 if(browserVersion.indexOf('IE') >= 0) {
-browserVersion[0] = 'Internet';
-browserVersion[1] = 'Explorer';
+  browserVersion[0] = 'Internet';
+  browserVersion[1] = 'Explorer';
 
 }
 
