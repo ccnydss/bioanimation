b301ec4085a1504e1266311ca9a452bf9b1613f0
Alexander Matson <alexmat2on@protonmail.com>
2019-02-25 11:17:17 -0500

    check for updates

--- index.js
+++ index.js
@@ -9,8 +9,43 @@ function createWindow () {
   win = new BrowserWindow({ width: 1280, height: 980, icon: path.join(__dirname, "/img/icon.png"), toolbar: false })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+
+  // Check for new updates
+   autoUpdater.checkForUpdatesAndNotify();
+}
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
 }
 
 app.on('ready', createWindow);
 app.on('window-all-closed', app.quit);
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+});
+
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
