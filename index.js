02d2047449233cc05d75d28d4fb9464a5e63287d
Alexander Matson <alexmat2on@protonmail.com>
2019-02-25 14:05:09 -0500

    explicitly enable toolbar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let win;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 1280, height: 980, icon: path.join(__dirname, "/img/icon.png") })
+  win = new BrowserWindow({ width: 1280, height: 980, icon: path.join(__dirname, "/img/icon.png"), toolbar: true })
 
   // and load the index.html of the app.
   win.loadFile('index.html');
