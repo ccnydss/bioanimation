10163f6fea5e94da132f73388ff8eedb40f5e3d0
Alexander Matson <alexmat2on@protonmail.com>
2019-02-27 12:43:32 -0500

    document and test point class

--- js/obj/animation/Point.js
+++ js/obj/animation/Point.js
@@ -1,18 +1,29 @@
-/** Point class */
+/** Class representing a point on the canvas. */
 class Point {
+  /**
+   * Create a new point
+   * @param {number} x - Coordinate X value
+   * @param {number} y - Coordinate Y value
+   */
   constructor(_x, _y) {
     this.x = _x;
     this.y = _y;
   }
 
+  /**
+   * Function to draw the point on a p5js canvas.
+   */
   draw() {
     stroke(255);
     point(this.x, this.y);
   }
 
+  /**
+   * Compute the Euclidean distance between this point and another point.
+   * @param {Point} p - The external point whose distance from us we want to find.
+   * @returns {number}
+   */
   distance(p) {
-    // input: another Point;
-    // output: compute how far away P is from this
     var xdiff = this.x - p.x;
     var ydiff = this.y - p.y;
     var result = Math.sqrt((xdiff * xdiff) + (ydiff * ydiff))
--- js/tests/point_tests.js
+++ js/tests/point_tests.js
@@ -12,3 +12,12 @@ QUnit.test("constructor", function(assert) {
   assert.equal(p3.x, -2, "p3 x coord = -2");
   assert.equal(p3.y, -6, "p3 y coord = -6");
 });
+
+QUnit.test("distance", function(assert) {
+  var p1 = new Point(0, 0);
+  var p2 = new Point(1, 0);
+  var p3 = new Point(3, 4);
+
+  assert.equal(p1.distance(p2), 1, "distance between p1 and p2 is correct");
+  assert.equal(p1.distance(p3), 5, "distance between p1 and p3 is correct");
+})
