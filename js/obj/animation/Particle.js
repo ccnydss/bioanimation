f0f729be584672f17d11a49a69b53a2ac10a9681
Alexander Matson <alexmat2on@protonmail.com>
2019-03-13 16:20:59 -0400

    document Particle parent class

--- js/obj/animation/Particle.js
+++ js/obj/animation/Particle.js
@@ -1,63 +1,182 @@
 class Particle {
   /**
-   * Particles are the main class of the application. They possess a radius,
-   * color, position (maintained by their centers), and velocity. Every time a
-   * particle is drawn to the screen, its position vector is incremented by
-   * the velocity vector. There are 3 types of particles: Cl-, K+, and Na+ ions.
-   *
-   * @param {Point} _center - The starting position of the new Particle
-   *
-   * @param {integer} _diam - The diameter of the particle
-   *
-   * @param {Point} _vel - The input velocity is represented as a Point object
-   *                       with (x, y) components.
-   *
-   * @param {boolean} _collidable - Determines if the particle will be impacted
-   *                                 by collisions with Container objects.
-   *
-   * @param {Color|string} _color - A p5 Color object/string.
-   *
-   * @param {boolean} [_display=true] - Determines if the particle object should be
-   *                               drawn to the screen at all.
-   */
+  * Particles are the main class of the application. They possess a radius,
+  * color, position (maintained by their centers), and velocity. Every time a
+  * particle is drawn to the screen, its position vector is incremented by
+  * the velocity vector. There are 3 types of particles: Cl-, K+, and Na+ ions.
+  *
+  * @param {Point} _center - The starting position of the new Particle
+  *
+  * @param {integer} _diam - The diameter of the particle
+  *
+  * @param {Point} _vel - The input velocity is represented as a Point object
+  *                       with (x, y) components.
+  *
+  * @param {boolean} _collidable - Determines if the particle will be impacted
+  *                                 by collisions with Container objects.
+  *
+  * @param {Color|string} _color - A p5 Color object/string.
+  *
+  * @param {boolean} [_display=true] - Determines if the particle object should be
+  *                               drawn to the screen at all.
+  */
   constructor(_center, _diam, _vel, _collidable, _color, _display=true) {
     /**
-      * @protected
-      * @type {Point}
-      */
+    * @protected
+    * @type {Point}
+    */
     this.center = _center;
 
     /**
-      * @private
-      * @type {integer}
-      */
+    * @private
+    * @type {integer}
+    */
     this.diameter = _diam;
 
     /**
-      * @private
-      * @type {integer}
-      */
+    * @private
+    * @type {integer}
+    */
     this.radius = int(_diam / 2);
 
+    /**
+    * @private
+    * @type {boolean}
+    */
     this.collidable = _collidable;
 
+    /**
+    * @private
+    * @type {boolean}
+    */
     this.display = _display;
-    this.m_color = _color;
 
-    this.m_speed = 5;
-    this.m_velocity = createVector(_vel.x, _vel.y).setMag(this.m_speed);
+    /**
+    * @private
+    * @type {Color|string}
+    */
+    this.fill_color = _color;
+
+    /**
+    * @private
+    * @type {integer}
+    */
+    this.speed = 5;
+
+    /**
+    * @private
+    * @type {p5.Vector}
+    */
+    this.m_velocity = createVector(_vel.x, _vel.y).setMag(this.speed);
   }
 
-  color(c = this.m_color) {
-    this.m_color = c;
+  /**
+  * Set and get the fill color of the particle.
+  *
+  * @param {Color | string} [c=this.fill_color] - The color to set the particle to
+  * @returns {Color | string} - The particle's new or current color
+  */
+  color(c = this.fill_color) {
+    this.fill_color = c;
     noStroke();
     fill(c);
+
+    return this.fill_color;
   }
 
+  /**
+  * Set and get the fill color of the particle.
+  *
+  * @param {integer} [xc=this.center.x] - The x-coordinate to draw the particle at. Defaults to the center point's x-value.
+  * @param {integer} [yc=this.center.y] - The y-coordinate to draw the particle at. Defaults to the center point's y-value.
+  * @param {integer} [d=this.diameter] - The diameter of the Particle circle that gets drawn. Defaults to member variable-specified diameter.
+  */
   draw(xc = this.center.x, yc = this.center.y, d = this.diameter) {
     if (this.display) ellipse(xc, yc, d);
   }
 
+  /**
+  * Method to determine if the given particle is close to, or nearby, a single
+  * point within a given radius (range) of pixels.
+  *
+  * @param {Point} point - The point to compare this one to.
+  * @param {integer} [range=0] - The distance (in pixels) that specifies how
+  *  far away the point should be from the Particle's boundary to trigger
+  *  this function. Defaults to 0 (i.e., checks to see if the Point is actually within this Particle).
+  */
+  nearToPoint(point, range=0) {
+    var distanceBetween = this.center.distance(point);
+    return distanceBetween <= this.radius + range;
+  }
+
+  /**
+  * Public method to set the particle's velocity. It enforces the speed of the
+  * of the particle by modifying the input vector's magnitude no matter what it
+  * was initially.
+  *
+  * @param {p5.Vector} in_vector - The P5 vector object to use.
+  */
+  setVelocity(in_vector) {
+    this.m_velocity = in_vector.setMag(this.speed);
+  }
+
+  /**
+  * Public method to set the particle's speed. This will be used to force all
+  * velocity vectors to maintain this given speed.
+  *
+  * @param {integer} speed - The speed to use, in terms of pixels per frame.
+  * Typically, something like 1-5 is pretty good.
+  */
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+
+  /**
+  * Public method to set the particle's display property.
+  *
+  * @param {boolean} disp - True/False for display the particle or not.
+  */
+  setDisplay(disp) {
+    this.display = disp;
+    setClassMember(this, "display", disp);
+  }
+
+  /**
+  * Public method to set the particle's direction to a random value. Used
+  * during the equilibrium function to give particles a random direction
+  * after they cross the cell membrane.
+  *
+  * @param {boolean} topTop - True/False if the particle's y-component is
+  * negative (moving upwards) or positive (moving downwards).
+  *
+  * @returns {p5.Vector} - The velocity vector that is going in this random direction.
+  */
+  randomDirection(toTop) {
+    var afterDirection;
+    if (toTop) {
+      afterDirection = randomFromRanges(
+        [
+          [ PI / 6 , PI / 3 ],
+          [ 2*PI / 3 , 5*PI / 6 ]
+        ]
+      )
+    } else {
+      afterDirection = randomFromRanges(
+        [
+          [ 7*PI / 6, 4*PI / 3],
+          [ 5*PI / 3, 11*PI / 6]
+        ]
+      )
+    };
+    return p5.Vector.fromAngle(afterDirection);
+  }
+
+  /**
+  * Move the particle within the context of a Container.
+  *
+  * @private
+  * @param {Container} container_context - The container (Rectangle) that contains this particle inside of it.
+  */
   move(container_context) {
     // Pass in a Container object the particle should be constrained inside.
     if (this.collidable) {
@@ -73,15 +192,25 @@ class Particle {
     this.draw();
   }
 
+  /**
+  * Move the particle within the context of a Container.
+  *
+  * @private
+  * @param {Container} container_context - The container (Rectangle) that contains this particle inside of it.
+  */
   moveCenter() {
     this.center.x += this.m_velocity.x;
     this.center.y += this.m_velocity.y;
   }
 
+  /**
+  * Detect if the particle is out of bounds of a Container or needs to be bounced.
+  * Reverse the velocity component if the particle collides with a wall.
+  *
+  * @private
+  * @param {Container} container - The container (Rectangle) that contains this particle inside of it.
+  */
   bounce(container) {
-    // Input: Particle
-    // Function: Reverse velocity components when particle collides with wall
-
     var bl = container.bl;
     var br = container.br;
     var tl = container.tl;
@@ -97,14 +226,13 @@ class Particle {
     this.moveCenter();
   }
 
-  nearToPoint(point, range=0) {
-    // Method to determine if the given particle is close to, or nearby, a single
-    // point within a given radius (range) of pixels.
-
-    var distanceBetween = this.center.distance(point);
-    return distanceBetween <= this.radius + range;
-  }
-
+  /**
+  * Detect if the particle is out of bounds of a Container. If it is, the particle
+  * is repositioned to the center of the container.
+  *
+  * @private
+  * @param {Container} container - The container (Rectangle) that contains this particle inside of it.
+  */
   checkOutOfBounds(container) {
     var outRight = this.center.x + this.radius > container.br.x;
     var outLeft = this.center.x - this.radius < container.tl.x;
@@ -117,6 +245,19 @@ class Particle {
     }
   }
 
+  /**
+  * Reverse the velocity component if the particle collides with a wall. Whether
+  * the x-component, y-component, or both of them get reversed depends on which
+  * wall is getting hit.
+  *
+  * @private
+  * @param {boolean} willCollide - A boolean corresponding to whether a particle
+  *   is touching or positioned past a given Container wall.
+  * @param {boolean} reverseX - Determines if we reverse the particle's x-component
+  *   (i.e., a vertical reflection).
+  * @param {boolean} reverseY - Determines if we reverse the particle's y-component
+  *   (i.e., a horizontal reflection).
+  */
   computeNewDirection(willCollide, reverseX, reverseY) {
     // Boolean function, and two bools
     // Check if the particle is about to collide and modify velocity appropriately
@@ -151,57 +292,48 @@ class Particle {
     }
   }
 
+  /**
+  * Detect if the particle has collided with or moved past the bottom of the
+  * container if it moves forward one more time (one more frame).
+  *
+  * @private
+  * @param {Point} bl - The bottom left point of the Container.
+  */
   nextPastBottom(bl) {
-    // Will particle cross bottom wall in next frame?
     return this.center.y + this.m_velocity.y + this.radius > bl.y;
   }
 
+  /**
+  * Detect if the particle has collided with or moved past the top of the
+  * container if it moves forward one more time (one more frame).
+  *
+  * @private
+  * @param {Point} tl - The top left point of the Container.
+  */
   nextPastTop(tl) {
-    // Will particle cross top wall in next frame?
     return this.center.y + this.m_velocity.y - this.radius < tl.y;
   }
 
+  /**
+  * Detect if the particle has collided with or moved past the right of the
+  * container if it moves forward one more time (one more frame).
+  *
+  * @private
+  * @param {Point} br - The bottom right point of the Container.
+  */
   nextPastRight(br) {
-    // Will particle cross right wall in next frame?
     return this.center.x + this.m_velocity.x + this.radius > br.x;
   }
 
+  /**
+  * Detect if the particle has collided with or moved past the left of the
+  * container if it moves forward one more time (one more frame).
+  *
+  * @private
+  * @param {Point} bl - The bottom left point of the Container.
+  */
   nextPastLeft(bl) {
-    // Will particle cross left wall in next frame?
     return this.center.x + this.m_velocity.x - this.radius < bl.x;
-  }
-
-  setDisplay(disp) {
-    this.display = disp;
-    setClassMember(this, "display", disp);
-  }
-
-  randomDirection(toTop) {
-    var afterDirection;
-    if (toTop) {
-      afterDirection = randomFromRanges(
-        [
-          [ PI / 6 , PI / 3 ],
-          [ 2*PI / 3 , 5*PI / 6 ]
-        ]
-      )
-    } else {
-      afterDirection = randomFromRanges(
-        [
-          [ 7*PI / 6, 4*PI / 3],
-          [ 5*PI / 3, 11*PI / 6]
-        ]
-      )
-    };
-    return p5.Vector.fromAngle(afterDirection);
-  }
-
-  setVelocity(in_vector) {
-    this.m_velocity = in_vector.setMag(this.m_speed);
-  }
-
-  setSpeed(speed) {
-    this.m_speed = speed;
   }
 }
 
