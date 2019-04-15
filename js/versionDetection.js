86a6598bc7842140a299da8a429b5ef55d61c95e
Chen Gao <40931390+chen0gao@users.noreply.github.com>
2019-04-15 12:05:56 -0400

    Console error #58 and UI updated (#74)
    * Initializing
    
    * chartist plot demo
    Optimizing the simulation performance #54
    
    * Change preview while pausing, will not update sim canvas #57
    
    * Switch back to chart.js
    
    * Console error #58
    
    * revised

--- js/versionDetection.js
+++ js/versionDetection.js
@@ -45,20 +45,31 @@ if (typeof require === "function") {
       var userOS = getOS();
       var releaseLink;
 
+      var user_platfrom;
+
       switch(userOS) {
-        case "Mac OS":
-        releaseLink = res.assets[0].browser_download_url;
+        case "Mac OS":;
+        document.getElementById('mac-tag').style.color = '#f39c12';
         break;
         case "Linux":
-        releaseLink = res.assets[1].browser_download_url;
+        document.getElementById('linux-tag').style.color = '#f39c12';
         break;
         case "Windows":
-        releaseLink = res.assets[2].browser_download_url;
+        document.getElementById('windows-tag').style.color = '#f39c12';
         break;
       }
 
       console.log("hmmmmm", releaseLink);
 
-      document.getElementById("downloadLink").innerHTML = "<a href='" + releaseLink + "'>v" + latestNum + "</a>"
+      var releaseLink_mac = res.assets[0].browser_download_url;
+      var releaseLink_linux = res.assets[1].browser_download_url;
+      var releaseLink_win = res.assets[2].browser_download_url;
+
+      var latestNum = res.name
+
+      document.getElementById("downloadLink-win").innerHTML = "<a href='" + releaseLink_win + "'>v" + latestNum + "<i class='fas fa-download'></i></a>"
+      document.getElementById("downloadLink-mac").innerHTML = "<a href='" + releaseLink_mac + "'>v" + latestNum + "<i class='fas fa-download'></i></a>"
+      document.getElementById("downloadLink-linux").innerHTML = "<a href='" + releaseLink_linux + "'>v" + latestNum + "<i class='fas fa-download'></i></a>"
+
     })
   }
